9a489216ace9e65e761c72e5a0e0e29f42d2f48e
Quentin Moulinier <qmoulinier@wemanity.com>
2019-05-27 13:09:50 +0200

    display browser (current folder path)

--- src/app/layout/metric/metric-routing.module.ts
+++ src/app/layout/metric/metric-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { MetricComponent } from './metric.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: MetricComponent
-    }
+  { path: '', component: MetricComponent},
+  { path: ':pathLevel1', component: MetricComponent},
+  { path: ':pathLevel1/:pathLevel2', component: MetricComponent},
+  { path: ':pathLevel1/:pathLevel2/:pathLevel3', component: MetricComponent},
 ];
 
 @NgModule({
